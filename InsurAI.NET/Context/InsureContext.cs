0aff9ac0f2d64b4dacafe3f3806aa12595dd6969
Esra Koç <esrakc239@gmail.com>
2025-11-13 17:16:46 +0300

    add entites and view

---
+++ InsurAI.NET/Context/InsureContext.cs
@@ -0,0 +1,33 @@
+﻿using InsurAI.NET.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsurAI.NET.Context
+{
+    public class InsureContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-G55JK8H;initial catalog=InsureDb; integrated security=true;trust server certificate=true");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<AboutItem> AboutItems { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<PricingPlan> PricingPlans { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<TrailerVideo> TrailerVideos { get; set; }
+        //public DbSet<Comment> Comments { get; set; }
+        //public DbSet<PricingPlanItem> PricingPlanItems { get; set; }
+        //public DbSet<Gallery> Galleries { get; set; }
+        //public DbSet<ClaudeAIMessage> ClaudeAIMessages { get; set; }
+        //public DbSet<Revenue> Revenues { get; set; }
+        //public DbSet<Expense> Expenses { get; set; }
+        //public DbSet<Policy> Policies { get; set; }
+
+    }
+}
---
+++ InsurAI.NET/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeadComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsurAI.NET.ViewComponents.AdminLayoutViewComponents
+{ 
+    // ViewComponent'ler, ASP.NET Core'da bir sayfanın belirli kısmını (örneğin header, footer, sidebar gibi)
+    // dinamik olarak render etmeye yarayan küçük, yeniden kullanılabilir bileşenlerdir.
+    public class _AdminLayoutHeadComponentPartial : ViewComponent
+    {
+        // Bu metot çalıştığında, ilgili ViewComponent'in çıktısını üretir.
+        // Örneğin bu "head" kısmı, layout’un <head> bölümüne CSS/JS, meta tag gibi içerikler ekleyebilir.
+        public IViewComponentResult Invoke()
+        {
+            // View() metodu, bu ViewComponent'e ait Razor View dosyasını render eder.
+            // Yani /Views/Shared/Components/_AdminLayoutHeadComponentPartial/Default.cshtml dosyasını çağırır.
+            return View();
+        }
+    }
+}
